9bf826b743a962b0e26c282dca2ffc911f01e905
Danilo Junior <c201830@ardentmills.com>
2021-11-17 13:19:50 -0300

    Add Hubs/ChamadoHub.cs

---
+++ SenaiChamados/Hubs/ChamadoHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SenaiChamados.Hubs
+{
+    public class ChamadoHub: Hub
+    {
+        public async Task Announce(string chamado)
+        {
+            //Clients.All.SendAsync();
+        }
+    }
+}
